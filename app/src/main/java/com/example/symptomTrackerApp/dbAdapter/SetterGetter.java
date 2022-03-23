7e8b7589f792776d02a82356db15d2e4c4eb45c6
Poonam Maywad <pmaywad@sundevils.asu.edu>
2022-03-22 19:29:21 -0700

    new changes by Akhil

--- app/src/main/java/com/example/symptomTrackerApp/dbAdapter/SetterGetter.java
+++
@@ -1,8 +0,0 @@
-package com.example.symptomTrackerApp.dbAdapter;
-
-public class SetterGetter {
-
-    public static final String userName="pmaywad";
-    public static final String passWord="pmaywad123";
-
-}
